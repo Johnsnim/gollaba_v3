a33ca81d4280c72afc9af580425ef35c6c6f589c
Johnsnim <qw506799@naver.com>
2024-09-02 03:42:08 +0900

    로그인 페이지 퍼블리싱

--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { GithubLogo, Logo } from "../asset";
+import { GithubLogo, KakaoLogo, Logo } from "../asset";
 
 const Login = () => {
   return (
@@ -15,6 +15,14 @@ const Login = () => {
             <img src={GithubLogo} className="ProviderIcon" alt="GithubLogo" />
             <p className="StartWithProvider">Github 계정으로 시작하기</p>
           </div>
+
+          <div className="LoginBox">
+            <img src={KakaoLogo} className="ProviderIcon" alt="GithubLogo" />
+            <p className="StartWithProvider"> Kakao 계정으로 시작하기</p>
+          </div>
+        </div>
+        <div className="Copyright">
+          © 2024 Team Gollaba All rights reserved.
         </div>
       </div>
     </div>
